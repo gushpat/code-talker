d2c4ed4faa92a58eab27d57d745fe92a7321a151
ghpvampiro <gustavo_hp@hotmail.com>
2022-02-06 20:23:52 -0300

    Create footer.php

---
+++ src/footer.php
@@ -0,0 +1,18 @@
+
+<?php
+
+  
+$footer = 
+"
+<Center> 
+<hr>
+<p href='#' style='font-weight: bold; font-size: 20px;' >Desenvolvido por Gustavo Patricio</p>
+
+<p style='font-weight: bold; font-size: 17px;'>Made with ❤️ in Sorocaba-SP || Copyright &copy; 2022 Pluvo App</p>
+<hr>
+</Center>";
+
+//echo $footer;
+
+
+?>
