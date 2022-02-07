1469d2300410b7aa9004d666a512cafa7d71666f
ghpvampiro <gustavo_hp@hotmail.com>
2022-02-07 02:03:02 -0300

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -5,12 +5,10 @@
 $footer = 
 "
 <Center> 
-<hr>
+
 <p href='#' style='font-weight: bold; font-size: 20px;' >Desenvolvido por Gustavo Patricio</p>
 
 <p style='font-weight: bold; font-size: 17px;'>Made with ❤️ in Sorocaba-SP || &copy; 2022 Code Talker</p>
-
-<p> Musica criada por: <a href='https://www.fesliyanstudios.com/' target='_blank'>Fesliyan Studios</a> </p>
 
 </Center>
 
