5f1a4f9fb06ee3e8fd5c11dba198e7362eb6570d
ghpvampiro <gustavo_hp@hotmail.com>
2022-02-08 00:44:13 -0300

    Create footer.php

---
+++ assets/footer.php
@@ -0,0 +1,21 @@
+
+<?php
+
+  
+$footer = 
+"
+<Center> 
+
+<p href='#' style='font-weight: bold; font-size: 20px;' >Desenvolvido por Gustavo Patricio</p>
+
+<p style='font-weight: bold; font-size: 17px;'>Made with ❤️ in Sorocaba-SP || &copy; 2022 Code Talker</p>
+
+</Center>
+
+
+";
+
+//echo $footer;
+
+
+?>
