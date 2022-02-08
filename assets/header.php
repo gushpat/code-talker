25fe051a694cb1599de2bc7cd0c15fab9aac688b
ghpvampiro <gustavo_hp@hotmail.com>
2022-02-08 00:44:15 -0300

    Create header.php

---
+++ assets/header.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$header = 
+'
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Inicio</a>
+  <a href="#news">Sobre</a>
+  <a href="#contact">Código Fonte</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+
+
+';
+
+
+?>
