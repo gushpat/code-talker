bef9ed4d2b3edabbab1d3a158ddb30e2daf4c661
Gustavo Patricio <gustavo_hp@hotmail.com>
2022-04-18 17:12:34 -0300

    Update footer.php

--- assets/footer.php
+++ assets/footer.php
@@ -7,9 +7,9 @@ $footer =
 
 
 <div class='footer'>
-    <p>&copy; Code Talker - versão 1.0</p>
+    <p>&copy; Code Talker - versão 1.0.2</p>
 
-<p href='github.com/gushpat' style='font-weight: bold; font-size: 20px;' >Desenvolvido por Gustavo Patricio</p>
+<p href='github.com/gushpat' style='font-weight: bold; font-size: 17px;' >Desenvolvido por Gustavo Patricio</p>
 
 <p style='font-weight: bold; font-size: 17px;'>Made with ❤️ in Sorocaba-SP || 2022 </p>
 
