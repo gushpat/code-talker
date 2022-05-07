34e62861da676305e55450148ae892092960bd9c
ghpvampiro <gustavo_hp@hotmail.com>
2022-05-06 21:15:49 -0300

    Update footer.php

--- assets/footer.php
+++ assets/footer.php
@@ -3,20 +3,32 @@
 
   
 $footer = 
-"
+'
 
 
-<div class='footer'>
-    <p>&copy; Code Talker - versão 1.0.2</p>
-
-<p href='github.com/gushpat' style='font-weight: bold; font-size: 17px;' >Desenvolvido por Gustavo Patricio</p>
-
-<p style='font-weight: bold; font-size: 17px;'>Made with ❤️ in Sorocaba-SP || 2022 </p>
-
-</div>
 
 
-";
+<div class="footer">
+      <p>
+        <strong>&copy; Code Talker - versão 1.1.0</strong> utiliza algebra linear para a criação da criptografia. <br>
+      </p>
+      <p>
+        <a>Criado por <a href="https://github.com/gushpat">Gustavo Patricio</a>.</a>
+
+        
+        <a>Desenvolvido em Sorocaba-SP com  ❤️ e <a href="https://www.php.net/">PHP</a>. </a>
+        </p>
+        <br>
+        O projeto é open source e seu código fonte disponível em meu repositório no <a href="https://github.com/gushpat/code-talker/">GitHub</a> e está sob a licença <a href="https://github.com/gushpat/code-talker/blob/main/LICENSE.md">GPL v2.0</a>.
+      
+    </div>
+
+
+';
+
+
+
 
 
 ?>
+
